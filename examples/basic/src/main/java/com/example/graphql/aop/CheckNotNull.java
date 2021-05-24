9a809b36f03609af28ca8889e99ef66d03d59507
Alexey Zhokhov <alexey@zhokhov.com>
2021-05-24 20:17:52 +0800

    Cleanup.

--- examples/basic/src/main/java/com/example/graphql/aop/CheckNotNull.java
+++ examples/basic/src/main/java/com/example/graphql/aop/CheckNotNull.java
@@ -7,7 +7,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 import static java.lang.annotation.ElementType.METHOD;
-import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
@@ -15,7 +14,7 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
  */
 @Documented
 @Retention(RUNTIME)
-@Target({TYPE, METHOD})
+@Target({METHOD})
 @Around
 public @interface CheckNotNull {
 }
