bdf1d99c9e0b7432e785201cce4c654bff87a238
Alexey Zhokhov <alexey@zhokhov.com>
2022-07-01 04:12:10 +0800

    Fix.

--- graphql-tools/src/test/groovy/io/micronaut/graphql/tools/exceptions/ClassNotIntrospectedExceptionSpec.groovy
+++ graphql-tools/src/test/groovy/io/micronaut/graphql/tools/exceptions/ClassNotIntrospectedExceptionSpec.groovy
@@ -119,3 +119,118 @@ input PriceInput {
     }
 
 }
+
+
+class ClassNotIntrospectedExceptionSpec3 extends AbstractTest {
+
+    static final String SPEC_NAME = "ClassNotIntrospectedExceptionSpec3"
+
+    void "test root resolver use not introspected class as input argument"() {
+        given:
+            @Language("GraphQL")
+            String schema = """
+schema {
+  query: Query
+}
+
+type Query {
+  price(input: PriceInput!): Float
+}
+
+input PriceInput {
+  from: String!
+  to: String!
+}
+"""
+
+            startContext(schema, SPEC_NAME)
+
+        when:
+            executeQuery("{month}")
+
+        then:
+            def e = thrown(BeanInstantiationException)
+            e.cause instanceof ClassNotIntrospectedException
+            e.cause.message == """The class ${PriceInput.name} is not introspected. Ensure the class is annotated with ${GraphQLInput.name}.
+  GraphQL type: Query
+  GraphQL field: price
+  Mapped class: ${Query.name}
+  Mapped method: price(${PriceInput.name} input)"""
+            e.cause.mappingDetails.graphQlType == 'Query'
+            e.cause.mappingDetails.graphQlField == 'price'
+            e.cause.mappingDetails.mappedClass == Query
+            e.cause.mappingDetails.mappedMethod == "price(${PriceInput.name} input)"
+    }
+
+    @Requires(property = 'spec.name', value = SPEC_NAME)
+    @GraphQLRootResolver
+    static class Query {
+        Float price(PriceInput input) {
+            return 10.00f
+        }
+    }
+
+    @GraphQLInput
+    static interface PriceInput {
+        String from
+        String to
+    }
+
+}
+
+
+class ClassNotIntrospectedExceptionSpec4 extends AbstractTest {
+
+    static final String SPEC_NAME = "ClassNotIntrospectedExceptionSpec4"
+
+    void "test root resolver use not introspected class as input argument"() {
+        given:
+            @Language("GraphQL")
+            String schema = """
+schema {
+  query: Query
+}
+
+type Query {
+  price(input: PriceInput!): Float
+}
+
+input PriceInput {
+  from: String!
+  to: String!
+}
+"""
+
+            startContext(schema, SPEC_NAME)
+
+        when:
+            executeQuery("{month}")
+
+        then:
+            def e = thrown(BeanInstantiationException)
+            e.cause instanceof ClassNotIntrospectedException
+            e.cause.message == """The class ${PriceInput.name} is not introspected. Ensure the class is annotated with ${GraphQLInput.name}.
+  GraphQL type: Query
+  GraphQL field: price
+  Mapped class: ${Query.name}
+  Mapped method: price(${PriceInput.name} input)"""
+            e.cause.mappingDetails.graphQlType == 'Query'
+            e.cause.mappingDetails.graphQlField == 'price'
+            e.cause.mappingDetails.mappedClass == Query
+            e.cause.mappingDetails.mappedMethod == "price(${PriceInput.name} input)"
+    }
+
+    @Requires(property = 'spec.name', value = SPEC_NAME)
+    @GraphQLRootResolver
+    static class Query {
+        Float price(PriceInput input) {
+            return 10.00f
+        }
+    }
+
+    @GraphQLInput
+    static enum PriceInput {
+        TEST
+    }
+
+}
