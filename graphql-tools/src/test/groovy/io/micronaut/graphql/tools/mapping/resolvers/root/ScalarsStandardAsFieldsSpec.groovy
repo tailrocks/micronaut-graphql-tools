1de931ea3b3201aa7c3b0c911505d5ef1eca4414
Alexey Zhokhov <alexey@zhokhov.com>
2022-07-02 17:21:21 +0800

    Fix.

--- graphql-tools/src/test/groovy/io/micronaut/graphql/tools/mapping/resolvers/root/ScalarsStandardAsFieldsSpec.groovy
+++ graphql-tools/src/test/groovy/io/micronaut/graphql/tools/mapping/resolvers/root/ScalarsStandardAsFieldsSpec.groovy
@@ -1,12 +1,13 @@
-package io.micronaut.graphql.tools
+package io.micronaut.graphql.tools.mapping.resolvers.root
 
 import io.micronaut.context.annotation.Requires
+import io.micronaut.graphql.tools.AbstractTest
 import io.micronaut.graphql.tools.annotation.GraphQLRootResolver
 import org.intellij.lang.annotations.Language
 
-class ScalarsSpec1 extends AbstractTest {
+class ScalarsStandardAsFieldsSpec extends AbstractTest {
 
-    static final String SPEC_NAME = "ScalarsSpec1"
+    static final String SPEC_NAME = "ScalarsStandardAsFieldsSpec"
 
     void "test mapping standard graphql scalars"() {
         given:
@@ -56,7 +57,7 @@ type Query {
             result.data.testID == 'id'
     }
 
-    void "test mapping standard graphql scalars [required field]"() {
+    void "test mapping standard graphql scalars [required fields]"() {
         given:
             @Language("GraphQL")
             String schema = """
@@ -137,135 +138,6 @@ type Query {
 
         String testID() {
             return "id"
-        }
-    }
-
-}
-
-
-class ScalarsSpec2 extends AbstractTest {
-
-    static final String SPEC_NAME = "ScalarsSpec2"
-
-    void "test mapping extended graphql-java scalars"() {
-        given:
-            @Language("GraphQL")
-            String schema = """
-scalar Long
-scalar Short
-scalar BigDecimal
-scalar BigInteger
-
-schema {
-  query: Query
-}
-
-type Query {
-  testLong1: Long
-  testLong2: Long
-  testShort1: Short
-  testShort2: Short
-  testBigDecimal: BigDecimal
-  testBigInteger: BigInteger
-}
-"""
-
-            startContext(schema, SPEC_NAME)
-
-        when:
-            def result = executeQuery("""
-{
-    testLong1
-    testLong2
-    testShort1
-    testShort2
-    testBigDecimal
-    testBigInteger
-}
-""")
-
-        then:
-            result.errors.isEmpty()
-            result.data.testLong1 == Long.MAX_VALUE
-            result.data.testLong2 == Long.MIN_VALUE
-            result.data.testShort1 == Short.MAX_VALUE
-            result.data.testShort2 == Short.MIN_VALUE
-            result.data.testBigDecimal == BigDecimal.ZERO
-            result.data.testBigInteger == BigInteger.ONE
-    }
-
-    void "test mapping extended graphql-java scalars [required field]"() {
-        given:
-            @Language("GraphQL")
-            String schema = """
-scalar Long
-scalar Short
-scalar BigDecimal
-scalar BigInteger
-
-schema {
-  query: Query
-}
-
-type Query {
-  testLong1: Long!
-  testLong2: Long!
-  testShort1: Short!
-  testShort2: Short!
-  testBigDecimal: BigDecimal!
-  testBigInteger: BigInteger!
-}
-"""
-
-            startContext(schema, SPEC_NAME)
-
-        when:
-            def result = executeQuery("""
-{
-    testLong1
-    testLong2
-    testShort1
-    testShort2
-    testBigDecimal
-    testBigInteger
-}
-""")
-
-        then:
-            result.errors.isEmpty()
-            result.data.testLong1 == Long.MAX_VALUE
-            result.data.testLong2 == Long.MIN_VALUE
-            result.data.testShort1 == Short.MAX_VALUE
-            result.data.testShort2 == Short.MIN_VALUE
-            result.data.testBigDecimal == BigDecimal.ZERO
-            result.data.testBigInteger == BigInteger.ONE
-    }
-
-    @Requires(property = 'spec.name', value = SPEC_NAME)
-    @GraphQLRootResolver
-    static class Query {
-        long testLong1() {
-            return Long.MAX_VALUE
-        }
-
-        Long testLong2() {
-            return Long.MIN_VALUE
-        }
-
-        short testShort1() {
-            return Short.MAX_VALUE
-        }
-
-        Short testShort2() {
-            return Short.MIN_VALUE
-        }
-
-        BigDecimal testBigDecimal() {
-            return BigDecimal.ZERO
-        }
-
-        BigInteger testBigInteger() {
-            return BigInteger.ONE
         }
     }
 
