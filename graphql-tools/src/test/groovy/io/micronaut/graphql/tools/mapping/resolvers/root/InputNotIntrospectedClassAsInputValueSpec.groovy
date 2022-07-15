ad7c132a738fc325388f0995bec9a60f91eca2a2
Alexey Zhokhov <alexey@zhokhov.com>
2022-07-16 00:25:49 +0800

    Fix.

---
+++ graphql-tools/src/test/groovy/io/micronaut/graphql/tools/mapping/resolvers/root/InputNotIntrospectedClassAsInputValueSpec.groovy
@@ -0,0 +1,73 @@
+package io.micronaut.graphql.tools.mapping.resolvers.root
+
+import io.micronaut.context.annotation.Requires
+import io.micronaut.context.exceptions.BeanInstantiationException
+import io.micronaut.graphql.tools.AbstractTest
+import io.micronaut.graphql.tools.annotation.GraphQLInput
+import io.micronaut.graphql.tools.annotation.GraphQLRootResolver
+import io.micronaut.graphql.tools.exceptions.ClassNotIntrospectedException
+import org.intellij.lang.annotations.Language
+
+class InputNotIntrospectedClassAsInputValueSpec extends AbstractTest {
+
+    static final String SPEC_NAME = "InputNotIntrospectedClassAsInputValueSpec"
+
+    void "test TODO"() {
+        given:
+            @Language("GraphQL")
+            String schema = """
+schema {
+  query: Query
+}
+
+type Query {
+  price(input: PriceInput): Float
+}
+
+input PriceInput {
+  conversion: ConversionInput!
+}
+
+input ConversionInput {
+  from: String!
+  to: String!
+}
+"""
+            startContext(schema, SPEC_NAME)
+
+        when:
+            getGraphQLBean()
+
+        then:
+            def e = thrown(BeanInstantiationException)
+            e.cause instanceof ClassNotIntrospectedException
+            e.cause.message == """The class ${ConversionInput.name} is not introspected. Ensure the class is annotated with ${GraphQLInput.name}.
+  GraphQL input object type: PriceInput
+  GraphQL input value: conversion
+  Mapped class: ${PriceInput.name}
+  Mapped property: conversion"""
+            e.cause.mappingContext.graphQlInputObjectType == 'PriceInput'
+            e.cause.mappingContext.graphQlInputValue == 'conversion'
+            e.cause.mappingContext.mappedClass == PriceInput
+            e.cause.mappingContext.mappedProperty == 'conversion'
+    }
+
+    @Requires(property = 'spec.name', value = SPEC_NAME)
+    @GraphQLRootResolver
+    static class Query {
+        Float price(PriceInput input) {
+            return 10.00f
+        }
+    }
+
+    @GraphQLInput
+    static class PriceInput {
+        ConversionInput conversion
+    }
+
+    static class ConversionInput {
+        String from
+        String to
+    }
+
+}
